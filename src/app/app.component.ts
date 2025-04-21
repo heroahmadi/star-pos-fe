0982658b6c058d1468df7aa71a58e919f45e597d
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2025-04-21 21:23:21 +0700

    create footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, HeaderComponent],
+  imports: [SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/sidebar/sidebar.component.ts
+++ src/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'sidebar',
+    selector: 'sidebar-component',
     templateUrl: './sidebar.component.html'
 })
 
