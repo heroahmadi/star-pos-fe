b4cd661a82a00aee0f485fb3518911cad7896be5
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2025-04-21 21:18:05 +0700

    create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
