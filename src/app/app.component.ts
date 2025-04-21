9fd6d9cb21e2888009cadea4e595320a7151ceec
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2025-04-21 21:13:35 +0700

    delete unused codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
