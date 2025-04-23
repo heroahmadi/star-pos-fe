c4d24608729c2915ff4d39036a2f620bfa103542
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2025-04-23 11:39:52 +0700

    add basic route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, HeaderComponent, FooterComponent],
+  imports: [SidebarComponent, HeaderComponent, FooterComponent, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: ProductComponent},
+  {path: '**', redirectTo: ''}
+];
