c803002cda8b3b45cf5c9930d1e8b86f2a43ab9e
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2025-04-23 11:25:05 +0700

    move components under app directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from "../sidebar/sidebar.component";
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
